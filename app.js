8075c6a56b407e4d0445d21e23eea916e54dabad
tachyla <tachylad.murray@gmail.com>
2017-04-14 09:17:17 -0700

    no error

--- app.js
+++ app.js
@@ -18,22 +18,22 @@ function displayStations(stations){
 	stations.forEach(station => console.log(station.name + ' ' + station.free_bikes));
 }
 
-function calcDistance(lat1, lon1, lat2, lon2){
-	var R = 6371e3; // metres
-	var φ1 = lat1.toRadians();
-	var φ2 = lat2.toRadians();
-	var Δφ = (lat2-lat1).toRadians();
-	var Δλ = (lon2-lon1).toRadians();
+// function calcDistance(lat1, lon1, lat2, lon2){
+// 	var R = 6371e3; // metres
+// 	var φ1 = lat1.toRadians();
+// 	var φ2 = lat2.toRadians();
+// 	var Δφ = (lat2-lat1).toRadians();
+// 	var Δλ = (lon2-lon1).toRadians();
 
-	var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-	        Math.cos(φ1) * Math.cos(φ2) *
-	        Math.sin(Δλ/2) * Math.sin(Δλ/2);
-	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+// 	var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+// 	        Math.cos(φ1) * Math.cos(φ2) *
+// 	        Math.sin(Δλ/2) * Math.sin(Δλ/2);
+// 	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
 
-	return R * c;	
-}
+// 	return R * c;	
+// }
 
-console.log(calcDistance())
+//console.log(calcDistance())
 //state modification functions
 // 	only contain javascript no jquery
 
