878173bf1bc567a683608558fb3dd585c8d6aef9
tachyla <tachylad.murray@gmail.com>
2017-04-15 13:58:47 -0700

    freeBikesFilter function evaluates true or false

--- app.js
+++ app.js
@@ -19,12 +19,10 @@ function displayStations(stations){
 	$(".resultsContainer").empty();
 	stations.forEach(function(station){
 		console.log(station.name + ' ' + station.free_bikes);
-		let resultsHTML = `${station.name} ${station.free_bikes}<br>`;
+		let resultsHTML = `${station.name} has ${station.free_bikes} bikes<br>`;
 		$(".resultsContainer").append(resultsHTML);
 	});
 }
-
-//function display
 
 // //render function 
 // 	where the html elements are added
@@ -35,19 +33,19 @@ function displayStations(stations){
 $(function(){
 	$("form").on("submit", function(event){
 		event.preventDefault();
-		const cityName = $("#location_label").val();
+		const cityName = $("#city_needed").val();
 	 	const numFreeBikes = parseInt($("#bikes_needed").val());	
 		console.log('CITY:',cityName, numFreeBikes, 'and type is', typeof numFreeBikes);
-
-
-
 
 		fetchAllNetworks(cityName, function(data){
 			const usNetworks = data.networks.filter(network => network.location.country === 'US');
 			const cityNetwork = usNetworks.filter(network => network.location.city.toLowerCase() == cityName.toLowerCase());
 			console.log(cityNetwork);
+			//cityNetwork contains only locations in that city
+
 			const uri = 'http://api.citybik.es';
 			let cityNetworkUrl = uri + cityNetwork[0].href;
+			console.log(cityNetworkUrl);
 
 			// usNetworks.forEach(val => {
 			// 	cityNetworkUrls.push(uri + val.href)
@@ -57,18 +55,25 @@ $(function(){
 			// 	cityNetworkUrls.push(uri + val.href)
 			// });
 
-
 			// console.log(cityNetwork, cityNetworkUrls);
-			console.log(cityNetworkUrl);
 
 			let availableBikes;
+
 			function freeBikesFilter(val){
+				//console.log(val);
+				console.log("test");
 				return val.free_bikes >= numFreeBikes;
 			};
 
 			$.getJSON(cityNetworkUrl, function(val){
-				availableBikes = val.network.stations.filter(freeBikesFilter);
-				console.log(availableBikes);
+				availableBikes = val.network.stations.forEach(function(station){
+					//console.log(station.free_bikes);
+					console.log(freeBikesFilter(station));
+				})
+				
+				//THIS didnt touch the correct value for AVAILABLE BIKES
+				//availableBikes = val.network.stations.filter(freeBikesFilter);
+				console.log("This station has: "  + availableBikes +  " available bikes");
 			});
 
 			// const availableBikes = cityNetwork.filter(network => )
