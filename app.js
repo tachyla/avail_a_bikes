97a1ed99d582da20e0ab5685356569508a964e30
pham-thien <pham.thien@outlook.com>
2017-04-15 23:45:10 -0400

    add showStartScreen function

--- app.js
+++ app.js
@@ -14,44 +14,56 @@ function fetchNetwork(id, callback){
 	$.getJSON(baseUrl + '/' + id, callback);
 }
 
-function displayStations(stations){
+function showStartScreen() {
+	let startScreenHTML = `
+			<header class="header_container">
+					<h1>Avail-a-Bikes</h1>
+					<h2>Let us get you from A -to- B</h2>
+			</header>
+			<div class="logo_container">
+				<img class="logo" src="avail-a-bikes.jpg" />
+			</div>
+			<button type="submit" class="start_button">Find Bikes</button>`;
+	$(".outer_container").append(startScreenHTML);
+}
+
+function displayStations(stations, choice){
 	console.log(stations);
 	$(".resultsContainer").empty();
 
+	let userInputScreenHTML = `
+					<header class="header_container">
+						<h1>Avail-a-Bikes</h1>
+						<h2>Let us get you from A -to- B</h2>
+					</header>
+					<div class="main_container">
+						<form>
+							<label class="search">Search</label>
+							<input type="text" id="city_needed" placeholder="San Francisco">
+							<input type="text" id="bikes_needed" name="numberofppl_label" placeholder="5">
+						
+				 			<button id="searchbutton">Submit</button>
+						</form>
+						<section class="resultsContainer">RESULTS CONTAINER BELOW</section>
+					</div>`;
+
 	stations.forEach(function(station){
 		if(station.free_bikes >= $('#bikes_needed').val()) {
-		console.log(station.name + ' ' + station.free_bikes);
-		let resultsHTML = `${station.name} has ${station.free_bikes} bikes<br>`;
-		$(".resultsContainer").append(resultsHTML);
-		let startScreenHTML = `
-					<header class="header_container">
-							<h1>Avail-a-Bikes</h1>
-							<h2>Let us get you from A -to- B</h2>
-					</header>
-					<div class="logo_container">
-						<img class="logo" src="avail-a-bikes.jpg" />
-					</div>
-					<button type="submit" class="start_button">Find Bikes</button>`;
-		let userInputScreenHTML = `
-						<header class="header_container">
-							<h1>Avail-a-Bikes</h1>
-							<h2>Let us get you from A -to- B</h2>
-						</header>
-						<div class="main_container">
-							<form>
-								<label class="search">Search</label>
-								<input type="text" id="city_needed" placeholder="San Francisco">
-								<input type="text" id="bikes_needed" name="numberofppl_label" placeholder="5">
-							
-					 			<button id="searchbutton">Submit</button>
-							</form>
-							<section class="resultsContainer">RESULTS CONTAINER BELOW</section>
-						</div>`;
+			console.log(station.name + ' ' + station.free_bikes);
+		 	$(".resultsContainer").append(resultsHTML);
 		}
+		let resultsHTML = `${station.name} has ${station.free_bikes} bikes<br>`;
 	});
 }
 
+showStartScreen();
+
 $(function(){
+	// displayStations(station, "startScreen");
+	// $(".outer_container").on("click", ".start_button", function(event) {
+	// 	displayStations(station, "startScreen");
+	// })
+
 	$("form").on("submit", function(event){
 		event.preventDefault();
 		const cityName = $("#city_needed").val();
@@ -68,48 +80,25 @@ $(function(){
 			let cityNetworkUrl = uri + cityNetwork[0].href;
 			console.log(cityNetworkUrl);
 
-			// usNetworks.forEach(val => {
-			// 	cityNetworkUrls.push(uri + val.href)
-			// });
-
-			// cityNetwork.forEach(val => {
-			// 	cityNetworkUrls.push(uri + val.href)
-			// });
-
-			// console.log(cityNetwork, cityNetworkUrls);
-
 			let availableBikes;
 
 			function freeBikesFilter(val){
 				if(val.free_bikes >= numFreeBikes){
-					//console.log(val.free_bikes);
 					return val.free_bikes;
 				}
-				//console.log(val);
 				console.log("freeBikesTest");
-				//return val.free_bikes >= numFreeBikes;
 			};
 
 			$.getJSON(cityNetworkUrl, function(val){
-				//https://api.citybik.es/v2/networks/relay-atlanta
 				availableBikes = val.network.stations.forEach(function(station){
 					//console.log(station.free_bikes);
 					//console.log(freeBikesFilter(station));
 				})
-				
-				//THIS didnt touch the correct value for AVAILABLE BIKES
-				//availableBikes = val.network.stations.filter(freeBikesFilter);
-				//console.log("This station has: "  + availableBikes +  " available bikes");
 			});
-
-			// const availableBikes = cityNetwork.filter(network => )
 
 			fetchNetwork(cityNetwork[0].id, function(data) {
 				displayStations(data.network.stations);
 			});
-		});
-
-		
+		});	
 	});
-
 });
