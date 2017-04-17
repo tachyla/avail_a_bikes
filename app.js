c323d3a6955e5b2b811af6cb4254dcbcdee81dbc
pham-thien <pham.thien@outlook.com>
2017-04-17 00:22:23 -0400

    add li styling

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //put inside object literal
-const appState = {baseUrl: "http://api.citybik.es/v2/networks"};
+const appState = {baseUrl: 'http://api.citybik.es/v2/networks'};
 
 
 /**
@@ -8,34 +8,34 @@ const appState = {baseUrl: "http://api.citybik.es/v2/networks"};
   @param {Function} callback - passes data response as first param
 */
 function fetchAllNetworks(cityName, data) {
-	$.getJSON(appState.baseUrl, function(data){
-		const usNetworks = data.networks.filter(network => network.location.country === 'US');
-		const cityNetwork = usNetworks.filter(network => network.location.city.toLowerCase() == cityName.toLowerCase());
+  $.getJSON(appState.baseUrl, function(data){
+    const usNetworks = data.networks.filter(network => network.location.country === 'US');
+    const cityNetwork = usNetworks.filter(network => network.location.city.toLowerCase() == cityName.toLowerCase());
 
-		console.log("cityNetwork", cityNetwork);
+    console.log('cityNetwork', cityNetwork);
 			//cityNetwork contains only locations in that city
-		if (cityNetwork.length > 0) {
-			const uri = 'http://api.citybik.es';
-			let cityNetworkUrl = uri + cityNetwork[0].href;
-			console.log(cityNetworkUrl);
+    if (cityNetwork.length > 0) {
+      const uri = 'http://api.citybik.es';
+      let cityNetworkUrl = uri + cityNetwork[0].href;
+      console.log(cityNetworkUrl);
 
-			fetchNetwork(cityNetwork[0].id, function(data) {
-				displayStations(data.network.stations);
-			});
-		}
-		else {
-			$(".resultsContainer").empty();
-			$(".resultsContainer").append(`<span>no results found</span>`);
-		}
-	});
+      fetchNetwork(cityNetwork[0].id, function(data) {
+        displayStations(data.network.stations);
+      });
+    }
+    else {
+      $('.resultsContainer').empty();
+      $('.resultsContainer').append('<span>no results found</span>');
+    }
+  });
 }
 
 function fetchNetwork(id, callback){
-	$.getJSON(appState.baseUrl + '/' + id, callback);
+  $.getJSON(appState.baseUrl + '/' + id, callback);
 }
 
 function showStartScreen() {
-	let startScreenHTML = `
+  let startScreenHTML = `
 			<header class="header_container">
 					<h1>Avail-a-Bikes</h1>
 					<h2>Let us get you from A -to- B</h2>
@@ -44,12 +44,12 @@ function showStartScreen() {
 				<img class="logo" src="avail-a-bikes.jpg" />
 			</div>
 			<button type="submit" class="start_button">Find Bikes</button>`;
-	$(".outer_container").append(startScreenHTML);
+  $('.outer_container').append(startScreenHTML);
 }
 
 function showInputScreen() {
-	$(".outer_container").empty();
-	let userInputScreenHTML = `
+  $('.outer_container').empty();
+  let userInputScreenHTML = `
 					<header class="header_container">
 						<h1>Avail-a-Bikes</h1>
 					</header>
@@ -64,26 +64,26 @@ function showInputScreen() {
 						</form>
 						<section class="resultsContainer"></section>
 					</div>`;
-	$(".user_input").append(userInputScreenHTML);				
+  $('.user_input').append(userInputScreenHTML);				
 }
 
 function displayStations(stations){
-	console.log(stations);
-	$(".resultsContainer").empty();
-	let resultsHTML = '';
-	let bikesNeeded = $('#bikes_needed').val();
+  console.log(stations);
+  $('.resultsContainer').empty();
+  let resultsHTML = '';
+  let bikesNeeded = $('#bikes_needed').val();
 
-	stations.forEach(function(station){
-		if(station.free_bikes >= bikesNeeded) {
-			console.log(station.name + ' ' + station.free_bikes);
+  stations.forEach(function(station){
+    if(station.free_bikes >= bikesNeeded) {
+      console.log(station.name + ' ' + station.free_bikes);
 		 	resultsHTML += `
 		 	<ul>
 				<li>${station.name.substring(0, 25)} has ${station.free_bikes} bikes<br>
-				${station.extra.address}</li>
+				\t ${station.extra.address}</li>
  			</ul>`;
-		}
-	});
-	$(".resultsContainer").append(resultsHTML);
+    }
+  });
+  $('.resultsContainer').append(resultsHTML);
 }
 
 // first thing that needs to be called
@@ -92,23 +92,23 @@ showStartScreen();
 $(function(){
 	
 
-	$(".outer_container").on("click", ".start_button", function(event) {
-		event.preventDefault();
-		showInputScreen();
-	});
+  $('.outer_container').on('click', '.start_button', function(event) {
+    event.preventDefault();
+    showInputScreen();
+  });
 	
 	// attach to something always on the page, form wasn't already there to fire event listener
-	$(".user_input").on("click", "#searchbutton", function(event){
-		event.preventDefault();
+  $('.user_input').on('click', '#searchbutton', function(event){
+    event.preventDefault();
 		// // displayStations(stations);
-		const cityName = $("#city_needed").val();
-	 	const numFreeBikes = parseInt($("#bikes_needed").val());	
-		console.log('CITY:',cityName, numFreeBikes, 'and type is', typeof numFreeBikes);
+    const cityName = $('#city_needed').val();
+	 	const numFreeBikes = parseInt($('#bikes_needed').val());	
+    console.log('CITY:',cityName, numFreeBikes, 'and type is', typeof numFreeBikes);
 		// debugger;
 		// put JSON request in directly
 
-		fetchAllNetworks(cityName, appState);		
-	});
+    fetchAllNetworks(cityName, appState);		
+  });
 });
 
 
