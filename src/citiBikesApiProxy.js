e82d052be0050d23ceb4fc6a1b7cc55197c81cab
TaChyla <tachyla.murray@leadingagile.com>
2022-11-03 14:28:46 -0400

    handleResponse filters US networks only

--- src/citiBikesApiProxy.js
+++ src/citiBikesApiProxy.js
@@ -1,39 +1,19 @@
 const axios = require('axios');
 
 module.exports = function fetchAllNetworks(cityName) {  
-    function filterUsCallback(array) {
-        array.filter(element => console.log(element));
-    }
+    
+   function getNetworks() {
 
-    async function getNetworks() { //promise #1
-        // promise#1 resolved
-        try {
-            const getNetworksResponseData = await axios.get('https://api.citybik.es/v2/networks')
-            .then(resp => {//promise #1
-                const networksResponseData = resp.data.networks;
+        const requestNetworks = axios.get('https://api.citybik.es/v2/networks')
+        .then(resp => handleResponse(resp.data.networks))
+        .catch((error) => console.error(error.resp.body))
 
-                try {//promise#2 resolved
-                    async function filteredUsNetworks(networksResponseData){
-                        const usNetworks = networksResponseData.filter(network => network.location.country == 'US')
-                        return usNetworks;
-                    }
-                }
-
-                //promise #2 rejected
-                catch(error) {
-                    console.error(error.networksResponseData.body);
-                }
-            });
-        
-
-        
-        
-        }
-         // promise#1 rejected
-        catch(error) {
-            console.error(error.resp.body);
+        function handleResponse(data) {
+            // console.log(data);
+            let results = data;
+            const filteredUsNetworks = results.filter(network => network.location.country == 'US');
+            console.log(filteredUsNetworks);
         }
     }
-     
     return getNetworks();
 }
