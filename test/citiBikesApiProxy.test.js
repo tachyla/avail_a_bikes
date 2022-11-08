7de00511a2d09337495b9addc54c457c4d8677db
TaChyla <tachyla.murray@leadingagile.com>
2022-11-08 12:50:14 -0500

    axios mocks response data accurately

--- test/citiBikesApiProxy.test.js
+++ test/citiBikesApiProxy.test.js
@@ -1,82 +1,162 @@
 const {getNetworkHrefByCity, getNetworkStationsByHref} = require("../src/citiBikesApiProxy.js");
-const mockAxios = require("axios");
+const axios = require("axios");
 
 jest.mock("axios");
 
 describe("citi Bikes Api", () => {
-    
-    describe("get network href by city", () => {
-        beforeEach(() => {
-            mockAxios.get.mockImplementationOnce(() => Promise.resolve( {data: {
-                "networks": [
-                    {
-                        "href": "/v2/networks/fake",
-                        "location": {
-                            "city": "Columbus",
-                            "country": "fakeCountry",
-                        },
-                    },
-                    {                        
-                        "href": "/v2/networks/fakePath",
-                        "location": {
-                            "city": "Miami, FL",
-                            "country": "US",
-                        },
-                    },
-                    {
-                        "href": "/v2/networks/cogo",
-                        "location": {
-                            "city": "Columbus, OH",
-                            "country": "US",
-                        },
-                    }
-                ]} 
-            }));
-        });
 
+    describe("get network href by city", () => {
         it('returns the network for a city', async () => {
+            // ARRANGE
+            const data = {"networks": [
+                {
+                    "href": "/v2/networks/fake",
+                    "location": {
+                        "city": "Columbus",
+                        "country": "fakeCountry",
+                    },
+                },
+                {                        
+                    "href": "/v2/networks/fakePath",
+                    "location": {
+                        "city": "Miami, FL",
+                        "country": "US",
+                    },
+                },
+                {
+                    "href": "/v2/networks/cogo",
+                    "location": {
+                        "city": "Columbus, OH",
+                        "country": "US",
+                    }
+                }
+            ]};
+            axios.get.mockResolvedValueOnce({"data": data}); 
+            
+            // ACTION
             const result = await getNetworkHrefByCity("Columbus");
+            
+            // ASSERTION
             expect(result).toBe("/v2/networks/cogo");
         });
 
         it('returns null if a city is not provided', async () => {
-            const result = await getNetworkHrefByCity(null);
-            expect(result).toBe(null);
+                 // ARRANGE
+                 const data = {
+                    "networks": [
+                        {
+                            "href": "/v2/networks/fake",
+                            "location": {
+                                "city": "Columbus",
+                                "country": "fakeCountry",
+                            },
+                        },
+                        {                        
+                            "href": "/v2/networks/fakePath",
+                            "location": {
+                                "city": "Miami, FL",
+                                "country": "US",
+                            },
+                        },
+                        {
+                            "href": "/v2/networks/cogo",
+                            "location": {
+                                "city": "Columbus, OH",
+                                "country": "US",
+                            }
+                        }
+                    ]
+                };
+                axios.get.mockResolvedValueOnce({"data": data}); 
+                
+                //ACTION
+                const result = await getNetworkHrefByCity(null);
+
+                //ASSERTION
+                expect(result).toBe(null);
         });
 
         it('returns null if a city does not exist', async () => {
+            // ARRANGE
+            const data = {
+                "networks": [
+                {
+                    "href": "/v2/networks/fake",
+                    "location": {
+                        "city": "Columbus",
+                        "country": "fakeCountry",
+                    },
+                },
+                {                        
+                    "href": "/v2/networks/fakePath",
+                    "location": {
+                        "city": "Miami, FL",
+                        "country": "US",
+                    },
+                },
+                {
+                    "href": "/v2/networks/cogo",
+                    "location": {
+                        "city": "Columbus, OH",
+                        "country": "US",
+                    }
+                }
+            ]};
+            axios.get.mockResolvedValueOnce({"data": data}); 
+        
+            //ACTION
             const result = await getNetworkHrefByCity("Fakecity");
+
+            // ASSERT
             expect(result).toBe(null);
         });
     });
     
     describe("get network stations by href", () => {
-        beforeEach(() => {
-            mockAxios.get.mockImplementation(() => Promise.resolve({data: {
-                "stations": [
-                {
-                    "empty_slots": 9,
-                    "free_bikes": 2,
-                    "name": "City Hall",
-                },
-                {
-                    "empty_slots": 2,
-                    "free_bikes": 13,
-                    "name": "High St & Warren"
-                }
-                ]}
-            }));
-        });
-        
         it('returns null when no href is provided', async () => {
+            const data = {
+                "stations": [
+                    {
+                        "empty_slots": 9,
+                        "free_bikes": 2,
+                        "name": "City Hall",
+                    },
+                    {
+                        "empty_slots": 2,
+                        "free_bikes": 13,
+                        "name": "High St & Warren"
+                    }
+                ]
+            };
+            axios.get.mockResolvedValueOnce({"data": data});
+        
            const result = await getNetworkStationsByHref(null);
             expect(result).toBe(null);
         });
 
         it("returns first station name that has at least N number of bikes ", async () => {
+            // ARRANGE
+            const data = {
+                "stations": [
+                    {
+                        "empty_slots": 9,
+                        "free_bikes": 2,
+                        "name": "City Hall",
+                    },
+                    {
+                        "empty_slots": 2,
+                        "free_bikes": 13,
+                        "name": "High St & Warren"
+                    }
+                ]
+            };
+            axios.get.mockResolvedValueOnce({"data": data});
             let numberOfBikes = 3;
-            const result = await getNetworkStationsByHref("/v2/networks/cogo", numberOfBikes);   
-            console.log(result);       
+            
+            // ACTION
+            const result = await getNetworkStationsByHref("/v2/networks/cogo", numberOfBikes);  
+            
+            // ASSERTION 
             expect(result).toBe("High St & Warren");
         });
     });
